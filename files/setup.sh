fcf2eb011463c2ac458bdd4a110b7294bd82ad27
John Walker Jr <24240323+jwalkerdev@users.noreply.github.com>
2020-12-12 22:17:19 -0600

    Update

--- files/setup.sh
+++ files/setup.sh
@@ -2,28 +2,24 @@
 
 sudo apt update
 sudo apt upgrade -y
+sudo apt update
 
 # Just enough x windows to correctly run pygame with touch input
 #sudo apt-get install --no-install-recommends xorg
 
-
 # Install pip
 sudo apt-get install -y python-pip python3-pip
-
-# Install RPi.GPIO
-sudo apt-get install -y python-dev python-rpi.gpio python3-dev python3-rpi.gpio
-# Install gpiozero for the tools and libs
-sudo apt-get install -y python-gpiozero python3-gpiozero
-
-sudo apt-get install -y --fix-missing python-pip
-sudo apt-get install -y --fix-missing python-picamera
-sudo apt-get install -y --fix-missing python-pygame
-sudo apt-get install -y --fix-missing python-opencv
 sudo apt-get install -y --fix-missing python3-pip
 sudo apt-get install -y --fix-missing python3-picamera
 sudo apt-get install -y --fix-missing python3-pygame
 sudo apt-get install -y --fix-missing python3-opencv
-
-sudo apt-get install -y --fix-missing omxplayer vlc
 sudo apt --fix-broken -y install
-sudo apt-get install -y --fix-missing omxplayer vlc
+
+# Create run script
+(
+cat <<'END_OF_CONTENT'
+#!/bin/bash
+sudo python3 /home/pi/code/pi-nv/camera-ui/pygame/pg-picam-ui.py
+END_OF_CONTENT
+) > ~/run.sh
+chmod +x ~/run.sh
